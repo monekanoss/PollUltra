64a143d46b732849dd74c76290a1c617cf4cb7ef
monekanoss <monekanoss@proton.me>
2025-10-27 20:40:15 +0100

    Add FHECounterAddresses.ts

---
+++ frontend/packages/site/abi/FHECounterAddresses.ts
@@ -0,0 +1,9 @@
+
+/*
+  This file is auto-generated.
+  By commands: 'npx hardhat deploy' or 'npx hardhat node'
+*/
+export const FHECounterAddresses = { 
+  "11155111": { address: "0x0000000000000000000000000000000000000000", chainId: 11155111, chainName: "sepolia" },
+  "31337": { address: "0x9Ab280b6D34E1B28be6D6bf09f19d9ab9d96F593", chainId: 31337, chainName: "hardhat" },
+};
