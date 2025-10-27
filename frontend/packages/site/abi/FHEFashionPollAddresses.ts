0213b667e43dc190eda15d43a6674bde3cc561ee
monekanoss <monekanoss@proton.me>
2025-10-27 20:40:17 +0100

    Add FHEFashionPollAddresses.ts

---
+++ frontend/packages/site/abi/FHEFashionPollAddresses.ts
@@ -0,0 +1,8 @@
+export const FHEFashionPollAddresses = {
+  "11155111": {
+    chainId: 11155111,
+    chainName: "Sepolia",
+    address: "0xbE6962010697f1B914166209a0E5B18A56bf5708",
+  },
+} as const;
+
